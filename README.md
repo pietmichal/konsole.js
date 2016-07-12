8ded0e45c0d29640b62662d7f4626dc873442b72
Michał Pietraszko <contact@harsay.com>
2016-07-12 22:22:51 +0100

    0.1.3 release

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # konsole.js page branch
 
-You can visit it here: http://harsay.com/konsole.js
+## [You can visit it here](http://harsay.github.io/konsole.js)
+## [Source code is here](https://github.com/harsay/konsole.js)
--- konsole.js
+++ konsole.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const utils = {
     htmlEntities: str => String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
 }
@@ -14,32 +16,37 @@ const cursor = {
         this.shown = true;
     },
     hide: function() {
-        konsole.consoleElement.innerHTML = konsole.consoleElement.innerHTML.slice(0, -1);
+        if (!this.shown) return;
+        konsole.consoleElement.innerHTML = konsole.consoleElement.innerHTML.replace(/_$/, '');
         this.shown = false;
     },
     start: function() {
+      if(konsole.enableInput)
         this.interval = setInterval(this.update.bind(this), 500);
     },
     stop: function() {
-        if (this.shown) this.hide();
+        this.hide();
         clearInterval(this.interval);
     }
 }
 
 const konsole = {
-    version: "0.1.2",
+    version: '0.1.3',
     consoleElement: null,
-    currentLine: "",
-    prompt: "> ",
-    init: function(element) { 
+    currentLine: '',
+    prompt: '>',
+    welcomeMessage: '',
+    enableInput: true,
+    init: function(element, welcomeMessage) {
         this.consoleElement = document.querySelector(element);
         this.consoleElement.setAttribute('style', 'overflow: auto; padding: 10px; box-sizing: border-box;');
+        this.welcomeMessage = welcomeMessage || '# konsole.js ' + this.version + ' - Type "help" to see available commands.\\n# Created by Michal Pietraszko (@theHarsay)\\n# Available on Github: github.com/Harsay/konsole.js';
 
-        document.onkeypress = this.processKeyCharacter.bind(this);
-        document.onkeydown = this.processActionKey.bind(this);
+        document.addEventListener('keypress', this.processKeyCharacter.bind(this));
+        document.addEventListener('keydown', this.processActionKey.bind(this));
 
-        this.writeLine('konsole.js ' + this.version + ' - Type "help" to see available commands');
-        this.write(this.prompt);
+        this.writeLine(this.welcomeMessage);
+        this.writePrompt();
 
         cursor.start();
 
@@ -47,7 +54,7 @@ const konsole = {
             if (args.length === 0 || !konsole.commandExists(args[0])) {
                 konsole.writeLine('Available commands:');
                 for (let command in konsole.commands) {
-                    konsole.writeLine(command);
+                    konsole.writeLine(command + ' - ' + konsole.commands[command].helpMessage);
                 }
             }
             else {
@@ -56,47 +63,69 @@ const konsole = {
         });
 
         this.createCommand('version', 'Shows konsole.js version.', function() {
-            konsole.writeLine('konsole.js version '+konsole.version);
+            konsole.writeLine('konsole.js version ' + konsole.version);
         });
 
         this.createCommand('print', 'Prints given text.', function(args) {
             konsole.writeLine(args.join(' '));
         });
+
+        this.createCommand('cls', 'clears screen', function() {
+            konsole.clearScreen();
+        });
+
+        this.createCommand('setPrompt', 'sets prompt', function(args) {
+            konsole.prompt = args.join(' ');
+        });
+
+        this.createCommand('asynctest', 'test asynchornous command', function(args) {
+            konsole.toggleInput();
+            let interval = setInterval(() => konsole.write('.'), 300);
+            setTimeout(() => {
+              konsole.writeLine(" Time's up. Let's do this.");
+              clearInterval(interval);
+              setTimeout(() => {
+                konsole.writeLine('LEEEEEEEROOOOOOY JEEEEEENKINNSSSSS!');
+                konsole.toggleInput();
+              }, 1000);
+            }, 1200);
+        });
     },
     processKeyCharacter: function(event) {
         let keyCode = event.which || event.keyCode;
-        if (keyCode === 13 || keyCode === 8) return;
+        if (!this.enableInput || keyCode === 13 || keyCode === 8) return;
 
         cursor.stop();
 
         let char = String.fromCharCode(keyCode);
         this.currentLine += char;
         this.write(char);
-        this.scrollViewToBottom();
 
         cursor.start();
     },
     processActionKey: function(event) {
-        if (this.currentLine.length === 0) return;
+        let keyCode = event.which || event.keyCode;
+
+        if (keyCode === 8) event.preventDefault();
+        if (!this.enableInput || this.currentLine.length === 0) return;
 
         cursor.stop();
-
-        let keyCode = event.which || event.keyCode;
 
         if (keyCode === 13) { // enter
             this.breakLine();
             this.parseCommand(this.currentLine);
-            this.write(this.prompt);
+            this.writePrompt();
+            cursor.start();
             this.currentLine = "";
         }
         else if (keyCode === 8) { // backspace
             this.currentLine = this.currentLine.slice(0, -1);
             this.consoleElement.innerHTML = this.consoleElement.innerHTML.slice(0, -1);
         }
-
-        this.scrollViewToBottom();
-
-        cursor.start();
+    },
+    writePrompt: function() {
+        if(this.enableInput)
+            this.write(this.prompt + ' ');
     },
     writeLine: function(str) {
         this.write(str);
@@ -104,6 +133,7 @@ const konsole = {
     },
     breakLine: function() {
         this.consoleElement.innerHTML += '<br>';
+        this.scrollViewToBottom();
     },
     hasLineBreak: function() {
         return this.consoleElement.innerHTML.substr(this.consoleElement.innerHTML.length - 4) === '<br>';
@@ -113,7 +143,8 @@ const konsole = {
             this.consoleElement.innerHTML = this.consoleElement.innerHTML.replace(/<br>$/, ''); // replace last occurence of <br> with empty string.
     },
     write: function(str) {
-        this.consoleElement.innerHTML += utils.htmlEntities(str);
+        this.consoleElement.innerHTML += utils.htmlEntities(str).replace(/\\n/g, '<br>');
+        this.scrollViewToBottom();
     },
     parseCommand: function(str) {
         let arr = str.split(' ');
@@ -138,7 +169,17 @@ const konsole = {
     scrollViewToBottom: function() {
         this.consoleElement.scrollTop = this.consoleElement.scrollHeight - this.consoleElement.clientHeight;
     },
+    toggleInput: function() {
+        this.enableInput = !this.enableInput;
+        if(!this.enableInput) {
+          cursor.stop();
+        } else {
+          this.writePrompt();
+          cursor.start();
+        }
+    },
+    clearScreen: function() {
+        this.consoleElement.innerHTML = "";
+    },
     commands: {}
 }
-
-
